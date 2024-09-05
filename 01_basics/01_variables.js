85e564025930db3574af505d3c1b250a9dbbd14f
hariom-cmd <56794092+hariom-cmd@users.noreply.github.com>
2024-09-05 19:24:01 +0000

    learn about basic data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const birthYear = 1999;            // Recomended data type
+let age = 24;                     // Commonly used data type
+var fullName = "Hariom Saini";   // Never use this variable type
+hariom = 24;                    // worst practice to declare variable like this
+
+//console.log(fullName);
+
+// A tabular way to show all variables
+console.table([birthYear,age,fullName,hariom]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
-console.log("hariom saini");
+const birthYear = 1999; 
+let age = 24;
+var fullName = "Hariom Saini";
+hariom = 24;
+
+console.log(birthYear);
