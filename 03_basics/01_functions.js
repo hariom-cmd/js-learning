d565dc209437901a838a13c20fff432fef585f50
hariom-cmd <56794092+hariom-cmd@users.noreply.github.com>
2024-09-09 18:19:11 +0000

    basic understanding of function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,4 +34,49 @@ function logInUserMessage(username = "JS"){ // default Value is JS
     return `${username} just logged in.`
 }
 
-console.log(logInUserMessage());
+// console.log(logInUserMessage("Hariom"));
+
+
+// PART 2 => 20th video
+// let do some practice on REST Operator
+
+function calculateCartPrice(val1, val2, ...otherValues){// first two values goes to first two variables and rest all goes to rest parameter
+    return otherValues;
+}
+
+//console.log(calculateCartPrice(200, 300, 400, 500, 600));
+
+
+// Passing object to a function as an argumnet
+
+const user = {
+    username: "Hariom Saini",
+    subTotal: 599
+}
+
+
+function handleObject(anyObject){
+    console.log(`Hello! ${anyObject.username} your shopping sub Total is: ${anyObject.subTotal}`);
+}
+
+// handleObject(user); // we can pass object after creating it 
+// handleObject({      // we can also pass obejct at calling time
+//     username: "Saini",
+//     subTotal: 699
+// })
+
+
+
+
+
+
+// Passing array to the function
+
+const myNewArray = [100, 200, 300];
+
+function returnArrayValue(getAnArray){
+    return getAnArray[0];
+}
+
+console.log(returnArrayValue(myNewArray)); // this is first create and pass into the function
+console.log(returnArrayValue([1212,1313, 555])); // this is direct pass an array
