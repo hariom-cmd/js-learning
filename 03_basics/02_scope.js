444b5fd9879a240629ca8838a4e5614177778405
hariom-cmd <56794092+hariom-cmd@users.noreply.github.com>
2024-09-09 19:26:45 +0000

    hoisting and function scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -16,3 +16,61 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// 2nd video
+
+function one(){
+    const username = "Hariom";
+
+    function two(){
+        const website = "Youtube";
+        console.log(username);
+    }
+
+    // console.log(website);
+    two();
+}
+
+one();
+
+
+
+if(true){
+    const username = "Hariom";
+
+    if(username === "Hariom"){
+        const website = "youtube";
+        console.log(username + website);
+    }
+
+    // console.log(website); => error
+}
+
+// console.log(username); => error
+
+
+
+// ******************* interesting concept **********************
+
+// Diferent ways to declare functions
+
+
+// Function declaration and call anywhere allowed
+addOne(4);
+function addOne(num){
+    return num + 1;
+}
+addOne(4);
+
+
+
+// this is also a function but some time calls Expression
+// but here function declaration must be first than call/Execute
+// else give ERROR
+// is concept ko khte h Hoisting
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);
