9c745e9159b357d0d3480497bafeb9fd2c30a714
hariom-cmd <56794092+hariom-cmd@users.noreply.github.com>
2025-08-10 11:13:16 +0000

    Knowledge added in DOM folder inside one.html file

--- pract.js
+++ pract.js
@@ -4,4 +4,41 @@ const myObj = {
     [mySymbol] : "myKey1"
 }
 
-console.log(myObj);
+// console.log(myObj);
+
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "C plus plus",
+        languageFileName: "Cpp"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    }
+];
+
+// myCoding.forEach( (item) => {
+//     console.log(item.languageFileName, item.languageName);
+    
+// } )
+
+myCoding.map( (item) => {
+    console.log(item.languageFileName);
+} )
+
+
+const myArray = [1,2,3,4,5,6];
+
+const sum = myArray.reduce( (acc,currVal) => {
+    return acc + currVal
+}, 0 )
+console.log(sum);
